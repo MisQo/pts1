75472b907f3b5f283aa1223ca8fe4b8a6dc0cfa6
MisQoF <michal.farnbauer@gmail.com>
2023-01-22 18:35:33 +0100

    add imports to Gamestate.py

--- sleepingqueens/GameState.py
+++ sleepingqueens/GameState.py
@@ -1,15 +1,17 @@
 from dataclasses import dataclass
 from typing import Optional
-
+from sleepingqueens.AwokenQueenPosition import AwokenQueenPosition
 from sleepingqueens.Card import Card
+from sleepingqueens.HandPosition import HandPosition
 from sleepingqueens.Queen import Queen
+from sleepingqueens.SleepingQueenPosition import SleepingQueenPosition
 
 
 @dataclass
 class GameState:
     numberOfPlayers: int
     onTurn: int
-    sleepingQueens: set[sleepingQueenPosition]
+    sleepingQueens: set[SleepingQueenPosition]
     cards: dict[HandPosition, Optional[Card]]
     awokenQueens: dict[AwokenQueenPosition, Queen]
     cardsDiscardedLastTurn: list[Card]
